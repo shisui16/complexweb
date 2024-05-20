1257aed5bb1871e3fbaa8f17bf10e772c6c559a5
shisui16 <thoratsarthak@gmail.com>
2024-05-20 16:37:33 +0530

    draft 2

--- script.js
+++ script.js
@@ -81,8 +81,8 @@ $(document).ready(function() {
   const edges = new vis.DataSet([
       { from: 1, to: 2 },
       { from: 1, to: 3 },
-      { from: 2, to: 4 },
-      { from: 2, to: 5 }
+      { from: 1, to: 4 },
+      { from: 1, to: 5 }
   ]);
 
   const container = document.getElementById('network');
